115453d04ca065d582f235e99ca0c5fb65a2c4ac
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2022-12-03 11:54:25 +0100

    Add compatibility with canDrive & part of bus mapper is ready

--- src/canController.cpp
+++ src/canController.cpp
@@ -32,55 +32,99 @@ bool CANBusController::connect() {
     return true;
 }
 
+// returns false if packet is not mapped
+bool CANBusController::mapPacketData(long packetID, uint8_t *packetData) {
+    switch (packetID) {
+        case 0xD1: // Speed & Brakes
+            // [0]? const 0
+            // [1]? const 0
+            // [2] 0 to FF probably break pressure but not just it
+            // [3]? const 0
+            brakePressure = (int) packetData[2] * 128;
+//            Serial.print(" - break pressure is ");
+//            Serial.print(brakePressure);
+            return true;
+        case 0x140: // Clutch, Acceleration, RPM
+            clutchDown = packetData[1] == 0x00 ? true : false;
+//            Serial.print(" - clutch is ");
+//            Serial.print(clutchDown);
+            return true;
+        case 0x360: // Temperatures, Battery voltage -
+            // [0]? some low values
+            // [1]? some low values
+            // [2]-40=oil temp
+            // [3]-40=coolant temp
+            // [4]-40=(probably) outdoor temp
+            // [5]? constant 0
+            // [6] is 145 (probably) it's battery voltage without dot 14.5
+            // [7]? constant 0
+            oilTemperature = (int) packetData[2] - 40;
+            coolantTemperature = (int) packetData[3] - 40;
+//            Serial.print(" - oli temp is ");
+//            Serial.print(oilTemperature);
+//            Serial.print(" - colant temp is ");
+//            Serial.print(coolantTemperature);
+            return true;
+        default:
+            return false;
+    }
+
+};
+
 int CANBusController::readPackets() {
     // try to parse packet
     int packetSize = CAN.parsePacket();
 
     if (packetSize) {
         // received a packet
-        Serial.print("Received ");
-
-        if (CAN.packetExtended()) {
-            Serial.print("extended ");
-        }
-
-        if (CAN.packetRtr()) {
-            // Remote transmission request, packet contains no data
-            Serial.print("RTR ");
-        }
-
-        Serial.print("packet with id 0x");
+//        Serial.print("Received ");
         long packetID = CAN.packetId();
         Serial.print(packetID, HEX);
 
         if (CAN.packetRtr()) {
-            Serial.print(" and requested length ");
+            // Remote transmission request, packet contains no data
+//            Serial.print("RTR ");
+            Serial.print(",01");
+        } else {
+            Serial.print(",00");
+        }
+
+        if (CAN.packetExtended()) {
+//            Serial.print("extended ");
+            Serial.print(",01,");
+        } else {
+            Serial.print(",00,");
+        }
+
+//        Serial.print("packet with id 0x");
+//        long packetID = CAN.packetId();
+//        Serial.print(packetID, HEX);
+
+        if (CAN.packetRtr()) {
+//            Serial.print(" and requested length ");
             Serial.println(CAN.packetDlc());
         } else {
-            Serial.print(" and length ");
-            Serial.println(packetSize);
+//            Serial.print(" and length ");
+            Serial.print(packetSize);
+//            Serial.print(" - value is: ");
 
-            if(packetID == 0xD1) {
-                Serial.println("\nBREAK PRESSURE CATCH: ");
-                uint8_t data[8];
-                int data_length = 0;
-                while (data_length < packetSize && data_length < sizeof(data)) {
-                    int byte_read = CAN.read();
-                    if (byte_read == -1) {
-                        break;
-                    }
-
-                    data[data_length++] = byteread;
+            uint8_t data[8];
+            int data_length = 0;
+            while (data_length < packetSize && data_length < sizeof(data)) {
+                int byte_read = CAN.read();
+                Serial.print((char)byte_read, HEX);
+                Serial.print(" ");
+                if (byte_read == -1) {
+                    break;
                 }
-                brakePressure = (int) data[2] * 128;
-                Serial.print(brakePressure);
-            } else {
-                // only print packet data for non-RTR packets
-                while (CAN.available()) {
-                    Serial.print((char)CAN.read(), HEX);
-                }
+                data[data_length++] = byte_read;
             }
-            Serial.println();
+            mapPacketData(packetID, data);
+
+//            // only print packet data for non-RTR packets
+//            while (CAN.available()) {
+//                Serial.print((char)CAN.read(), HEX);
+//            }
         }
         Serial.println();
     }
--- src/canController.h
+++ src/canController.h
@@ -7,9 +7,16 @@
 class CANBusController {
     public:
         int brakePressure = 0;
+        int oilTemperature = 0;
+        int coolantTemperature = 0;
+        bool clutchDown = false;
+
         CANBusController();
         bool connect();
         int readPackets();
+
+    private:
+        bool mapPacketData(long packetID,uint8_t* packetData);
 };
 
 
